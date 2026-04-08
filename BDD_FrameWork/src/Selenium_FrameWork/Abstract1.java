301a12b4ee9a21c8eea7b46b70bf00bb1a12367f
shubhamtest212 <shubhamjogdandtest@gmail.com>
2026-04-08 19:44:49 +0530

    NEW BRANCH

--- BDD_FrameWork/src/Selenium_FrameWork/Abstract1.java
+++ BDD_FrameWork/src/Selenium_FrameWork/Abstract1.java
@@ -15,5 +15,6 @@ public abstract class Abstract1 {
 		System.out.println("non-abstract method");
 		System.out.println("Hello SDET Engineer");
 		System.out.println("HELLO");
+		System.out.println("DONE");
 	}
 }
