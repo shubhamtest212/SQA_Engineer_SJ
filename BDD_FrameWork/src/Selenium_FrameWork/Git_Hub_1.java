b30da87812537359789ade54931fce3fcc665199
shubhamtest212 <shubhamjogdandtest@gmail.com>
2026-04-11 07:43:07 +0530

    New class code

---
+++ BDD_FrameWork/src/Selenium_FrameWork/Git_Hub_1.java
@@ -0,0 +1,26 @@
+package Selenium_FrameWork;
+
+import org.testng.annotations.Test;
+
+public class Git_Hub_1 {
+
+	@Test
+	public void aMethod() {
+		System.out.println("Hello a test method");
+	}
+
+	@Test
+	public void bMethod() {
+		System.out.println("Hello b test method");
+	}
+
+	@Test
+	public void cMethod() {
+		System.out.println("Hello c test method");
+	}
+
+	@Test
+	public void dMethod() {
+		System.out.println("Hello d test method");
+	}
+}
